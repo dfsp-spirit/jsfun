ec71293f594513dd4633e0a8c0a0a469607d4387
Tim Schäfer <dfsp-spirit@users.noreply.github.com>
2015-10-18 12:35:43 +0200

    add mapping for new chars

--- processingjs/imgtypewriter/mappings/split_image.sh
+++ processingjs/imgtypewriter/mappings/split_image.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
-## Splits an image into small tiles vertically (usage example: a screenshot of a single line of monospaced text is split into the (equally wide) characters.)
+﻿## Splits an image into small tiles vertically (usage example: a screenshot of a single line of monospaced text is split into the (equally wide) characters.)
+
+SRCIMG="$1"
+
+if [ -z "$1" ]; then
+  echo "split_image -- split an image into tiles."
+  echo "USAGE: $0 <image file>";
+  exit 1
+fi
+
 convert -crop 40x60 LiberationMono_Line1_40_60.png %d.png
