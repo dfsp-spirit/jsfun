0efb2d9a5336314f1e903cfcb9fb5e6113a1ba85
Tim Schaefer <ts+code@rcmd.org>
2018-10-05 15:46:32 +0200

    remove print statements, update license to MIT, fix minor doc issues.

--- p5js/liveplot/liveplot.js
+++ p5js/liveplot/liveplot.js
@@ -1,11 +1,13 @@
-// Liveplot.js -- a p5js scene by Tim Schäfer.
-// This is free software, published under the GPLv3 license. No warranties.
+// Liveplot.js -- an interactive p5js live plotting scene.
+// Copyright Tim Schäfer, 2018
+// This is free software, published under the MIT license. No warranties. See https://opensource.org/licenses/MIT
+// See https://github.com/dfsp-spirit/web/blob/master/p5js/liveplot/liveplot.js for the latest version.
 
 // +++ Settings +++
-var numDots = 50;   // number of rows of the field
-var numPositionsPerDot = 20;      // number of target positions per line, or columns (vertical lines)
-var numLines = 1;           // number of the moving, colorful plot lines at start of animation
-var numLinesMax = 20;     // max number of lines, only used if doAddLinesOverTime is true
+var numDots = 50; // number of rows of the field
+var numPositionsPerDot = 20; // number of target positions per line, or columns (vertical lines)
+var numLines = 1; // number of the moving, colorful plot lines at start of animation
+var numLinesMax = 20; // max number of lines, only used if doAddLinesOverTime is true
 var numPositionsToDrawForLine = 18;
 
 var doDrawPotentialTargetPoints = false;
@@ -40,47 +42,46 @@ var interDotPositionDistanceX;
 var nextLineYSpot;
 var numJumps = 0;
 var doDrawXLabels = false; // debug only
-var someColors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000' ];
+var someColors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
 
 function setup() {
-	frameRate(30);
+  frameRate(30);
   createCanvas(800, 400);
   textSize(useTextSize);
-	interDotDistanceY = (height/numDots);
-  interDotPositionDistanceX = (width/numPositionsPerDot);
+  interDotDistanceY = (height / numDots);
+  interDotPositionDistanceX = (width / numPositionsPerDot);
 
-	for (var i = 0; i < numDots; i++) {
+  for (var i = 0; i < numDots; i++) {
     var currentYPos = i * interDotDistanceY + 0.5 * interDotDistanceY;
     for (var j = 0; j <= numPositionsPerDot; j++) {
       var currentXPos = j * interDotPositionDistanceX + 0.5 * interDotPositionDistanceX;
-		    dotPositions.push([currentXPos, currentYPos]);
+      dotPositions.push([currentXPos, currentYPos]);
     }
-	}
+  }
 
 
 
-  maxDrawLineX = dotPositions[numPositionsToDrawForLine-2][0] + 0.5 * interDotPositionDistanceX;
+  maxDrawLineX = dotPositions[numPositionsToDrawForLine - 2][0] + 0.5 * interDotPositionDistanceX;
 
   nextLineYSpot = [];
   for (var k = 0; k < numLines; k++) {
     var currentLineYSpots = [];
     for (var l = 0; l < numPositionsToDrawForLine; l++) {
-      currentLineYSpots.push(getRandomInt(0, numDots-1));
+      currentLineYSpots.push(getRandomInt(0, numDots - 1));
     }
     linesYSpots.push(currentLineYSpots);
-    var nextRandomYSpot = getRandomYPositionPotentiallyWithMouseBonus(0, numDots-1);
+    var nextRandomYSpot = getRandomYPositionPotentiallyWithMouseBonus(0, numDots - 1);
     nextLineYSpot.push(nextRandomYSpot);
   }
 
   // fill alive data for lines
-  for(var b=0; b < numLinesMax; b++) {
+  for (var b = 0; b < numLinesMax; b++) {
     var currentLineAlive = [];
-    if(b < numLines) {
+    if (b < numLines) {
       for (var l = 0; l < numPositionsToDrawForLine; l++) {
         currentLineAlive.push(true);
       }
-    }
-    else {
+    } else {
       for (var l = 0; l < numPositionsToDrawForLine; l++) {
         currentLineAlive.push(false);
       }
@@ -96,90 +97,85 @@ function addLine() {
     currentLineYSpots.push(0);
   }
   linesYSpots.push(currentLineYSpots);
-  var nextRandomYSpot = getRandomYPositionPotentiallyWithMouseBonus(0, numDots-1);
+  var nextRandomYSpot = getRandomYPositionPotentiallyWithMouseBonus(0, numDots - 1);
   nextLineYSpot.push(nextRandomYSpot);
 }
+
 
 function draw() {
   background(backGroundColor);
 
-	// move Dots
+  // move Dots
   var jumpThisFrame = false;
-	for (var i = 0; i < dotPositions.length; i++) {
+  for (var i = 0; i < dotPositions.length; i++) {
     dotPositions[i][0] -= 2;
-		if(dotPositions[i][0] < -interDotPositionDistanceX) {  // reset if it left screen
-      var howMuchSmaller = -interDotPositionDistanceX -dotPositions[i][0];
-		  dotPositions[i][0] = width - howMuchSmaller;
+    if (dotPositions[i][0] < -interDotPositionDistanceX) { // reset if it left screen
+      var howMuchSmaller = -interDotPositionDistanceX - dotPositions[i][0];
+      dotPositions[i][0] = width - howMuchSmaller;
       jumpThisFrame = true;
-		}
-	}
+    }
+  }
 
-  if(jumpThisFrame) {
+  if (jumpThisFrame) {
     numJumps++;
-    if(doAddLinesOverTime && numJumps % 5 == 0 && numLines < numLinesMax) {
+    if (doAddLinesOverTime && numJumps % 5 == 0 && numLines < numLinesMax) {
       addLine();
     }
     // chose new random next points for all lines
     for (var k = 0; k < numLines; k++) {
       linesYSpots[k].shift();
       linesYSpots[k].push(nextLineYSpot[k]);
-      nextLineYSpot[k] = getRandomYPositionPotentiallyWithMouseBonus(0, numDots-1);
+      nextLineYSpot[k] = getRandomYPositionPotentiallyWithMouseBonus(0, numDots - 1);
     }
     // update alive data
     for (var b = 0; b < numLinesMax; b++) {
       linesAlive[b].shift();
-      if(b < numLines) {
+      if (b < numLines) {
         linesAlive[b].push(true);
-      }
-      else {
+      } else {
         linesAlive[b].push(false);
       }
     }
   }
 
-  if(doDrawYLabels || doDrawHorizontalGridLines) {
+  if (doDrawYLabels || doDrawHorizontalGridLines) {
     fill(textColor);
     for (var i = 0; i < numDots; i++) {
-      if(i > 0 && i % floor(numDots/10) == 0) {
+      if (i > 0 && i % floor(numDots / 10) == 0) {
         var currentYPos = i * interDotDistanceY + 0.5 * interDotDistanceY;
         noStroke();
-        text(""+i, width -15, currentYPos);
+        text("" + i, width - 15, currentYPos);
         stroke(horizontalLinesColor);
         line(0, currentYPos, width, currentYPos);
       }
     }
-	}
+  }
 
 
-  if(doDrawLineCount) {
+  if (doDrawLineCount) {
     noStroke();
     fill(textColor);
-    if(numLines < numLinesMax) {
-      text(""+numLines+" / " + numLinesMax, width -35, height -20);
-    }
-    else {
-      text(""+numLines, width -35, height -20);
+    if (numLines < numLinesMax) {
+      text("" + numLines + " / " + numLinesMax, width - 35, height - 20);
+    } else {
+      text("" + numLines, width - 35, height - 20);
     }
   }
 
   // draw maxDrawLineX
-  if(doDrawMaxDrawXLine) {
+  if (doDrawMaxDrawXLine) {
     stroke(color(0, 255, 0));
     line(maxDrawLineX, 0, maxDrawLineX, height);
   }
   stroke(color(0));
 
-	// draw dots
-	fill(color(255, 0, 0));
+  // draw dots
+  fill(color(255, 0, 0));
   if (doDrawPotentialTargetPoints) {
-	  for (var j = 0; j < dotPositions.length; j++) {
-		  ellipse(dotPositions[j][0], dotPositions[j][1], 5);
-	  }
+    for (var j = 0; j < dotPositions.length; j++) {
+      ellipse(dotPositions[j][0], dotPositions[j][1], 5);
+    }
   }
-
-
-
-  // advance Y spots
 
   // draw lines
   //  - find the current X positions of all points
@@ -187,12 +183,12 @@ function draw() {
   stroke(verticalLinesColor);
   for (var l = 0; l <= numPositionsPerDot; l++) {
     currentPointXPositions[l] = dotPositions[l][0];
-    if(doDrawXLabels) {
+    if (doDrawXLabels) {
       fill(textColor);
-      text(" "+l, currentPointXPositions[l], 20);
+      text(" " + l, currentPointXPositions[l], 20);
     }
     // draw vertical lines
-    if(doDrawVerticalGridLines) {
+    if (doDrawVerticalGridLines) {
       line(currentPointXPositions[l], 0, currentPointXPositions[l], height);
     }
   }
@@ -202,80 +198,77 @@ function draw() {
   // - draw lines between first numPositionsToDrawForLine points
 
   // draw sum
-  if(doDrawSumAtVerticalLineBottom || doDrawMean || doDrawStandardDeviation) {
+  if (doDrawSumAtVerticalLineBottom || doDrawMean || doDrawStandardDeviation) {
     fill(textColor);
     var numToSkipAtEnd = numPositionsPerDot - numPositionsToDrawForLine;
     var sumOfSums = 0;
-    for(var m=0; m < currentPointXPositions.sortIndices.length; m++) {
-      var idx=currentPointXPositions.sortIndices[m];
+    for (var m = 0; m < currentPointXPositions.sortIndices.length; m++) {
+      var idx = currentPointXPositions.sortIndices[m];
       var sumAtLinePosition = 0;
       var allValuesAtLinePosition = [];
-      for(var n=0; n < numLines; n++) {
-        if(linesAlive[n][idx]) {
+      for (var n = 0; n < numLines; n++) {
+        if (linesAlive[n][idx]) {
           sumAtLinePosition += linesYSpots[n][idx];
           allValuesAtLinePosition.push(linesYSpots[n][idx]);
         }
 
       }
       var meanVal = floor(sumAtLinePosition / allValuesAtLinePosition.length);
-      if(doDrawStandardDeviation) {
+      if (doDrawStandardDeviation) {
         var stdDev = allValuesAtLinePosition.stanDeviate();
         var meanValueYPos = meanVal * interDotDistanceY + 0.5 * interDotDistanceY;
 
         // draw vertical sddev line
         strokeWeight(10);
         stroke(color('rgba(255, 0, 0, 0.3)'));
-        line(currentPointXPositions[idx], meanValueYPos-stdDev, currentPointXPositions[idx], meanValueYPos+stdDev);
+        line(currentPointXPositions[idx], meanValueYPos - stdDev, currentPointXPositions[idx], meanValueYPos + stdDev);
 
         // draw short horizontal line mean line (inside the whide stddev line) with less alpha
         strokeWeight(2);
         stroke(color('rgba(255, 0, 0, 0.6)'));
-        line(currentPointXPositions[idx]-4, meanValueYPos, currentPointXPositions[idx]+4, meanValueYPos);
+        line(currentPointXPositions[idx] - 4, meanValueYPos, currentPointXPositions[idx] + 4, meanValueYPos);
       }
       noStroke();
 
-      var sumLabelToPlot = " "+sumAtLinePosition;
-      var meanLabelToPlot = " "+meanVal;
-      if(isNaN(sumAtLinePosition) || allValuesAtLinePosition.length == 0) {
+      var sumLabelToPlot = " " + sumAtLinePosition;
+      var meanLabelToPlot = " " + meanVal;
+      if (isNaN(sumAtLinePosition) || allValuesAtLinePosition.length == 0) {
         sumAtLinePosition = 0;
         sumLabelToPlot = "";
         meanLabelToPlot = "";
       }
-      if(doDrawSumAtVerticalLineBottom) {
+      if (doDrawSumAtVerticalLineBottom) {
         text(sumLabelToPlot, currentPointXPositions[idx], height - 5);
       }
-      if(doDrawMean) {
+      if (doDrawMean) {
         text(meanLabelToPlot, currentPointXPositions[idx], 10);
       }
       sumOfSums += sumAtLinePosition;
     }
     fill(textColor);
-    text("m="+round(sumOfSums/numLines), width - 35, height - 5);
+    text("m=" + round(sumOfSums / numLines), width - 35, height - 5);
   }
 
-// draw red marker in upper right corner to show user that mouse tracking is active
-if(doUseMouseAttraction && mouseRoughlyWithinFrame()) {
-  strokeWeight(2);
-  stroke(color(150, 0, 0));
-  fill(color(255, 0, 0));
-  ellipse(width -20, 20, 10);
-  strokeWeight(1);
-  stroke(color(255, 0, 0));
-  line(0, mouseY, width, mouseY);
-}
+  // draw red marker in upper right corner to show user that mouse tracking is active
+  if (doUseMouseAttraction && mouseRoughlyWithinFrame()) {
+    strokeWeight(2);
+    stroke(color(150, 0, 0));
+    fill(color(255, 0, 0));
+    ellipse(width - 20, 20, 10);
+    strokeWeight(1);
+    stroke(color(255, 0, 0));
+    line(0, mouseY, width, mouseY);
+  }
 
   for (var k = 0; k < numLines; k++) {
     var thisLineYSpots = linesYSpots[k]; // these are only the indices, i.e., row numbers
-    //print("Line " + k + " last y spots: " + thisLineYSpots.join(",") + ". Next=" + nextLineYSpot[k]);
 
     var thisLineYPositionsOfSpots = []; // y coordinates
     for (var l = 0; l < numPositionsToDrawForLine; l++) {
       thisLineYPositionsOfSpots[l] = thisLineYSpots[l] * interDotDistanceY + 0.5 * interDotDistanceY;
     }
-    //print("Line " + k + " y positions: " + thisLineYPositionsOfSpots.join(","));
 
-
-    var positionShare = currentPointXPositions[0] / width;     // make alpha depend on screen position (less tansparent the more it gets to the right)
+    var positionShare = currentPointXPositions[0] / width; // make alpha depend on screen position (less tansparent the more it gets to the right)
     var colorNoAlpha = color(someColors[k]);
     var newAlpha = 100 + positionShare * 155;
     var colorAlpha = color(red(colorNoAlpha), green(colorNoAlpha), blue(colorNoAlpha), newAlpha);
@@ -284,39 +277,94 @@ if(doUseMouseAttraction && mouseRoughlyWithinFrame()) {
     strokeWeight(4);
     line(0, thisLineYPositionsOfSpots[0], currentPointXPositions[0], thisLineYPositionsOfSpots[0]);
     for (var l = 0; l <= numPositionsToDrawForLine; l++) {
-      if(linesAlive[k][l]) {
-        positionShare = currentPointXPositions[l] / width;     // make alpha depend on screen position (less tansparent the more it gets to the right)
+      if (linesAlive[k][l]) {
+        positionShare = currentPointXPositions[l] / width; // make alpha depend on screen position (less tansparent the more it gets to the right)
         colorNoAlpha = color(someColors[k]);
         newAlpha = 100 + positionShare * 155;
         colorAlpha = color(red(colorNoAlpha), green(colorNoAlpha), blue(colorNoAlpha), newAlpha);
         stroke(colorAlpha);
-        line(currentPointXPositions[l], thisLineYPositionsOfSpots[l], currentPointXPositions[l+1], thisLineYPositionsOfSpots[l+1]);
+        line(currentPointXPositions[l], thisLineYPositionsOfSpots[l], currentPointXPositions[l + 1], thisLineYPositionsOfSpots[l + 1]);
       }
     }
     // Draw line towards next point: compute y position at x position maxDrawLineX
     var nextY = nextLineYSpot[k] * interDotDistanceY + 0.5 * interDotDistanceY;
-    var lastY = thisLineYPositionsOfSpots[numPositionsToDrawForLine-1];
-    var lastX = currentPointXPositions[numPositionsToDrawForLine-1];
+    var lastY = thisLineYPositionsOfSpots[numPositionsToDrawForLine - 1];
+    var lastX = currentPointXPositions[numPositionsToDrawForLine - 1];
     var nextX = currentPointXPositions[numPositionsToDrawForLine];
     var yDiff = nextY - lastY;
     var xDiff = nextX - lastX;
     var ascent = yDiff / xDiff;
     var xDiffToMaxDrawLineX = maxDrawLineX - lastX;
     var yAtmaxDrawLineX = lastY + (ascent * xDiffToMaxDrawLineX);
-    line(currentPointXPositions[numPositionsToDrawForLine-1], lastY, maxDrawLineX, yAtmaxDrawLineX);
+    line(currentPointXPositions[numPositionsToDrawForLine - 1], lastY, maxDrawLineX, yAtmaxDrawLineX);
     strokeWeight(1);
-    //print("Line " + k + ": ascent=" + ascent +", nextY=" + nextY +", lastY=" +lastY + ". lastX=" + lastX + " ,nextX=" + nextX + ". xDiff=" + xDiff + " (interDotPositionDistanceX=" + interDotPositionDistanceX + "), yDiff=" + yDiff);
 
-    if(doDrawCurrentPoints) {
-      fill(color(255,127,80));    // draw current dot in orange (stroke with current line's color)
+    if (doDrawCurrentPoints) {
+      fill(color(255, 127, 80)); // draw current dot in orange (stroke with current line's color)
       ellipse(lastX, lastY, 10);
     }
-    if(doDrawTargetPoints) {
-      fill(color(255, 255, 0));   // draw next target in yellow (stroke with current line's color)
+    if (doDrawTargetPoints) {
+      fill(color(255, 255, 0)); // draw next target in yellow (stroke with current line's color)
       ellipse(nextX, nextY, 10);
     }
   }
 }
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function mouseRoughlyWithinFrame() {
+  var border = 10;
+  return mouseX > border && mouseX < (width - border) && mouseY > border && mouseY < (height - border);
+}
+
+// get a random new y positon for a line (where it moves next). If mouse attraction is enabled, this is shifted towards the mouse position.
+function getRandomYPositionPotentiallyWithMouseBonus(min, max) {
+  var randomIndex = getRandomInt(min, max);
+  if (doUseMouseAttraction && mouseRoughlyWithinFrame()) {
+    // find y spot closest to mouse position
+    var minDistToMouse = Number.POSITIVE_INFINITY;
+    var minDistIndex = 0;
+    for (var p = 0; p < numDots; p++) {
+      var currentYPosDot = p * interDotDistanceY + 0.5 * interDotDistanceY;
+      if (abs(currentYPosDot - mouseY) < minDistToMouse) {
+        minDistToMouse = abs(currentYPosDot - mouseY);
+        minDistIndex = p;
+        minDistToTrackedMouseYPos = currentYPosDot;
+      }
+    }
+    // with a certain probability, draw move the random point towards the mouse
+    if (getRandomInt(1, 10) < 9) {
+      var distanceRandomIndexToClosestToMouseIndex = minDistIndex - randomIndex;
+      var distanceToMoveTowardsMouseIndex = floor(distanceRandomIndexToClosestToMouseIndex / 2);
+      var adjustedIndex = randomIndex + distanceToMoveTowardsMouseIndex
+      return adjustedIndex;
+    }
+  }
+  return randomIndex;
+}
+
+// function stanDeviate by cssimsek, see https://stackoverflow.com/questions/7343890/standard-deviation-javascript
+Array.prototype.stanDeviate = function() {
+  if (this.length == 0) {
+    return 0;
+  }
+  var i, j, total = 0,
+    mean = 0,
+    diffSqredArr = [];
+  for (i = 0; i < this.length; i += 1) {
+    total += this[i];
+  }
+  mean = total / this.length;
+  for (j = 0; j < this.length; j += 1) {
+    diffSqredArr.push(Math.pow((this[j] - mean), 2));
+  }
+  return (Math.sqrt(diffSqredArr.reduce(function(firstEl, nextEl) {
+    return firstEl + nextEl;
+  }) / this.length));
+};
 
 // function sortWithIndeces by Dave Aaron Smith, see https://stackoverflow.com/questions/3730510/javascript-sort-array-and-return-an-array-of-indicies-that-indicates-the-positi
 function sortWithIndeces(toSort) {
@@ -333,54 +381,3 @@ function sortWithIndeces(toSort) {
   }
   return toSort;
 }
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function mouseRoughlyWithinFrame() {
-  var border = 10;
-  return mouseX > border && mouseX < (width - border) && mouseY > border && mouseY < (height - border);
-}
-
-function getRandomYPositionPotentiallyWithMouseBonus(min, max) {
-  var randomIndex = getRandomInt(min, max);
-  if(doUseMouseAttraction && mouseRoughlyWithinFrame()) {
-    // find y spot closest to mouse position
-    var minDistToMouse = Number.POSITIVE_INFINITY;
-    var minDistIndex = 0;
-    for(var p=0; p < numDots; p++) {
-      var currentYPosDot = p * interDotDistanceY + 0.5 * interDotDistanceY;
-      if(abs(currentYPosDot - mouseY) < minDistToMouse) {
-        minDistToMouse = abs(currentYPosDot - mouseY);
-        minDistIndex = p;
-        minDistToTrackedMouseYPos = currentYPosDot;
-      }
-    }
-    // with a certain probability, draw move the random point towards the mouse
-    if(getRandomInt(1, 10) < 9) {
-      var distanceRandomIndexToClosestToMouseIndex = minDistIndex - randomIndex;
-      var distanceToMoveTowardsMouseIndex = floor(distanceRandomIndexToClosestToMouseIndex/2);
-      var adjustedIndex = randomIndex + distanceToMoveTowardsMouseIndex
-      return adjustedIndex;
-    }
-  }
-  return randomIndex;
-}
-// function by cssimsek, see https://stackoverflow.com/questions/7343890/standard-deviation-javascript
-Array.prototype.stanDeviate = function(){
-  if(this.length == 0) {
-    return 0;
-  }
-   var i,j,total = 0, mean = 0, diffSqredArr = [];
-   for(i=0;i<this.length;i+=1){
-       total+=this[i];
-   }
-   mean = total/this.length;
-   for(j=0;j<this.length;j+=1){
-       diffSqredArr.push(Math.pow((this[j]-mean),2));
-   }
-   return (Math.sqrt(diffSqredArr.reduce(function(firstEl, nextEl){
-            return firstEl + nextEl;
-          })/this.length));
-};
